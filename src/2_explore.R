dd6c98017fdef9f69396771afb90b40c8cfde58c
noahhhhhh <noah.xm.shaw@gmail.com>
2016-02-28 17:06:33 +1100

    explored

--- src/2_explore.R
+++ src/2_explore.R
@@ -1,8 +1,85 @@
 setwd("../../Volumes/Data Science/Google Drive/data_science_competition/kaggle/BNP_Paribas_Cardif_Claims_Management/")
 rm(list = ls()); gc();
 require(data.table)
+require(purrr)
+source("utilities/preprocess.R")
 
 load("../data/BNP_Paribas_Cardif_Claims_Management/RData/dt_all.RData")
 #######################################################################################
-## 1.0 read ###########################################################################
+## 1.0 explore ########################################################################
 #######################################################################################
+## check NAs
+ColNAs(dt.all, method = "mean")
+# ID target     v1     v2     v3     v4     v5     v6     v7     v8     v9    v10    v11    v12    v13    v14    v15 
+# 0.00   0.00   0.44   0.44   0.00   0.44   0.43   0.44   0.44   0.43   0.44   0.00   0.44   0.00   0.44   0.00   0.44 
+# v16    v17    v18    v19    v20    v21    v22    v23    v24    v25    v26    v27    v28    v29    v30    v31    v32 
+# 0.44   0.44   0.44   0.44   0.44   0.01   0.00   0.44   0.00   0.43   0.44   0.44   0.44   0.44   0.00   0.00   0.44 
+# v33    v34    v35    v36    v37    v38    v39    v40    v41    v42    v43    v44    v45    v46    v47    v48    v49 
+# 0.44   0.00   0.44   0.43   0.44   0.00   0.44   0.00   0.44   0.44   0.44   0.44   0.44   0.43   0.00   0.44   0.44 
+# v50    v51    v52    v53    v54    v55    v56    v57    v58    v59    v60    v61    v62    v63    v64    v65    v66 
+# 0.00   0.44   0.00   0.44   0.43   0.44   0.00   0.44   0.44   0.44   0.44   0.44   0.00   0.43   0.44   0.44   0.00 
+# v67    v68    v69    v70    v71    v72    v73    v74    v75    v76    v77    v78    v79    v80    v81    v82    v83 
+# 0.44   0.44   0.44   0.43   0.00   0.00   0.44   0.00   0.00   0.44   0.44   0.44   0.00   0.44   0.43   0.43   0.44 
+# v84    v85    v86    v87    v88    v89    v90    v91    v92    v93    v94    v95    v96    v97    v98    v99   v100 
+# 0.44   0.44   0.44   0.43   0.44   0.43   0.44   0.00   0.44   0.44   0.44   0.44   0.44   0.44   0.43   0.44   0.44 
+# v101   v102   v103   v104   v105   v106   v107   v108   v109   v110   v111   v112   v113   v114   v115   v116   v117 
+# 0.44   0.45   0.44   0.44   0.43   0.44   0.00   0.43   0.43   0.00   0.44   0.00   0.00   0.00   0.44   0.44   0.43 
+# v118   v119   v120   v121   v122   v123   v124   v125   v126   v127   v128   v129   v130   v131 
+# 0.44   0.44   0.44   0.44   0.44   0.44   0.43   0.00   0.44   0.44   0.43   0.00   0.44   0.44 
+
+## check class
+unlist(lapply(dt.all, class))
+# ID    target        v1        v2        v3        v4        v5        v6        v7        v8        v9       v10 
+# "integer" "numeric" "numeric" "numeric"  "factor" "numeric" "numeric" "numeric" "numeric" "numeric" "numeric" "numeric" 
+# v11       v12       v13       v14       v15       v16       v17       v18       v19       v20       v21       v22 
+# "numeric" "numeric" "numeric" "numeric" "numeric" "numeric" "numeric" "numeric" "numeric" "numeric" "numeric"  "factor" 
+# v23       v24       v25       v26       v27       v28       v29       v30       v31       v32       v33       v34 
+# "numeric"  "factor" "numeric" "numeric" "numeric" "numeric" "numeric"  "factor"  "factor" "numeric" "numeric" "numeric" 
+# v35       v36       v37       v38       v39       v40       v41       v42       v43       v44       v45       v46 
+# "numeric" "numeric" "numeric" "integer" "numeric" "numeric" "numeric" "numeric" "numeric" "numeric" "numeric" "numeric" 
+# v47       v48       v49       v50       v51       v52       v53       v54       v55       v56       v57       v58 
+# "factor" "numeric" "numeric" "numeric" "numeric"  "factor" "numeric" "numeric" "numeric"  "factor" "numeric" "numeric" 
+# v59       v60       v61       v62       v63       v64       v65       v66       v67       v68       v69       v70 
+# "numeric" "numeric" "numeric" "integer" "numeric" "numeric" "numeric"  "factor" "numeric" "numeric" "numeric" "numeric" 
+# v71       v72       v73       v74       v75       v76       v77       v78       v79       v80       v81       v82 
+# "factor" "integer" "numeric"  "factor"  "factor" "numeric" "numeric" "numeric"  "factor" "numeric" "numeric" "numeric" 
+# v83       v84       v85       v86       v87       v88       v89       v90       v91       v92       v93       v94 
+# "numeric" "numeric" "numeric" "numeric" "numeric" "numeric" "numeric" "numeric"  "factor" "numeric" "numeric" "numeric" 
+# v95       v96       v97       v98       v99      v100      v101      v102      v103      v104      v105      v106 
+# "numeric" "numeric" "numeric" "numeric" "numeric" "numeric" "numeric" "numeric" "numeric" "numeric" "numeric" "numeric" 
+# v107      v108      v109      v110      v111      v112      v113      v114      v115      v116      v117      v118 
+# "factor" "numeric" "numeric"  "factor" "numeric"  "factor"  "factor" "numeric" "numeric" "numeric" "numeric" "numeric" 
+# v119      v120      v121      v122      v123      v124      v125      v126      v127      v128      v129      v130 
+# "numeric" "numeric" "numeric" "numeric" "numeric" "numeric"  "factor" "numeric" "numeric" "numeric" "integer" "numeric" 
+# v131 
+# "numeric" 
+
+## summarise column class
+class <- unlist(lapply(dt.all, class))
+table((class)[!names(class) %in% c("ID", "target")])
+# factor integer numeric 
+# 19       4     108 
+
+## save the colmmns by class
+cols.factor <- names(class)[class == "factor"]
+cols.numeric <- names(class)[class == "numeric"][names(class)[class == "numeric"] != "target"]
+cols.integer <- names(class)[class == "integer"][names(class)[class == "integer"] != "ID"]
+
+## check unique values
+ColUnique(dt.all[, cols.factor, with = F])
+# v3   v22   v24   v30   v31   v47   v52   v56   v66   v71   v74   v75   v79   v91  v107  v110  v112  v113  v125 
+# 4 23420     5     8     4    10    13   131     3    12     3     4    18     8     8     3    23    38    91 
+
+# check tabular summary of all factor columns
+lapply(dt.all[, cols.factor, with = F], table)
+
+#######################################################################################
+## 2.0 save ###########################################################################
+#######################################################################################
+dt.explored <- dt.all
+save(dt.explored, cols.factor, cols.integer, cols.numeric, file = "../data/BNP_Paribas_Cardif_Claims_Management/RData/dt_explored.RData")
+
+
+
+
+
---
+++ utilities/preprocess.R
@@ -0,0 +1,164 @@
+require(data.table)
+############################################################################################
+## 1. ColNAs ###############################################################################
+############################################################################################
+## Intro: summarise the stats of NAs of columns in a data table
+## Args:
+##  dt(data.table): a data table
+##  method(character): "sum" - number of NAs; "mean" - NAs proportion of total
+##  output(character): "all" - return all the output; "nonZero" - return only non-zero output
+## Return(numeric): output of NA stats
+ColNAs <- function(dt, method = "sum", output = "all"){
+    out <- as.numeric()
+    if (method == "sum"){
+        out <- apply(dt, 2, function(x){sum(is.na(x))})
+    } else if (method == "mean"){
+        out <- apply(dt, 2, function(x){round(mean(is.na(x)), 2)})
+    } else {
+        return(F)
+    }
+    
+    if (output == "all"){
+        return(out)
+    } else if (output == "nonZero"){
+        return(out[out != 0])
+    } else {
+        return(F)
+    }
+}
+
+############################################################################################
+## 2. ColUnique ############################################################################
+############################################################################################
+## Intro: summarise the number of unique values of columns in a data table
+## Args:
+##  dt(data.table): a data table
+## Return(numeric): output of unique value stats
+ColUnique <- function(dt){
+    out <- apply(dt, 2, function(x){length(unique(x))})
+    return(out)
+}
+
+############################################################################################
+## 3. BinaryEncode #########################################################################
+############################################################################################
+## Intro: Binary Encoding for categorial features
+## Args:
+##  dt(data.table): a data table
+##  cols(a vector of characters): names of targeted columns
+## Return(data.table): output of a data table with the additional binary columns
+BinaryEncode <- function(dt, cols){
+    require(miscFuncs)
+    require(stringr)
+    for(col in cols){
+        # unique values dict
+        dict.uniq <- data.table(ID = rownames(unique(dt[, col, with = F]))
+                                , unique(dt[, col, with = F]))
+        # decimal to binary vector, e.g. 19 --> 10011
+        vec.dec <- bin(dim(dict.uniq)[1])
+        # length of the binary vector, e.g. 19 --> 10011 --> 5
+        num.len <- length(vec.dec)
+        # binary encoding
+        # ID corresponding to col
+        dt.col <- merge(dt[, col, with = F], dict.uniq, by = col, all.x = T)
+        dt.col[, ID := as.integer(ID)]
+        # encoded vector
+        vec.bin <- unlist(apply(dt.col[, "ID", with = F], 1, function(x)(str_pad(paste(bin(x), collapse = ""), num.len, side = "left", pad = "0"))))
+        # set up the col names and binary values
+        vec.col  <- as.character()
+        for (i in 1:num.len){
+            vec.col[i] <- paste(col, "_bin_", i, sep = "")
+            dt[, vec.col[i]:= as.integer(substr(vec.bin, i, i))]
+        }
+        dt <- dt[, !col, with = F]
+    }
+    return(dt)
+}
+
+############################################################################################
+## 4. ConvertNonNumFactorToNumFactor #######################################################
+############################################################################################
+## Intro: Convert non-numeric factor to a numeric factor
+## Args:
+##  dt(data.table): a data table
+##  cols(a vector of characters): name of targeted columns
+## Return(data.table): output of a data table with the additional binary columns
+ConvertNonNumFactorToNumFactor <- function(dt, cols){
+    # unique values dict
+    dict.uniq <- data.table(ID = rownames(unique(dt[, cols, with = F]))
+                            , unique(dt[, cols, with = F]))
+    # ID corresponding to cols
+    dt.col <- merge(dt, dict.uniq, by = cols, all.x = T)
+    # set the name of the new cols
+    colname <- paste(cols, "_toNum", sep = "")
+    setnames(dt.col, names(dt.col), c(names(dt.col)[-length(names(dt.col))], colname))
+    
+    return(dt.col)
+}
+
+############################################################################################
+## 4. ConvertNonNumFactorToOrderedNum ######################################################
+############################################################################################
+## Intro: Convert non-numeric factor to a ordered numeric values
+## e.g.: AA(100), BB(21), CC(33) --> map AA, BB, CC to 3, 1, 2
+## Args:
+##  dt(data.table): a data table
+##  cols(a vector of characters): name of targeted columns
+## Return(data.table): output of a data table with the ordered numeric values
+ConvertNonNumFactorToOrderedNum <- function(dt, cols){
+    require(plyr)
+    # get the list of tabular summary 
+    ls.table <- lapply(dt[, cols, with = F], table)
+    # get the list of names of the tablular summary
+    ls.names <- lapply(ls.table, function(x) names(x))
+    colnames <- paste(cols, "_toOrderedNum", sep = "")
+    # get the list of the ordred numeirc representing the tabluar summary
+    ls.ordered <- lapply(ls.table, function(x)(frank(as.vector(x), ties.method = "dense")))
+    
+    ls.replaced <- list()
+    for (col in cols){
+        n <- ls.names[[col]]
+        o <- ls.ordered[[col]]
+        ls.replaced[[paste(col, "toOrderedNum", sep = "")]] <- mapvalues(dt[[col]], from = n, to = o)
+    }
+    dt.replaced <- as.data.table(lapply(ls.replaced, print))
+    return(dt.replaced)
+}
+
+############################################################################################
+## 6. Noise ################################################################################
+############################################################################################
+## Intro: add noise into a data table. This function references Ivan Liu.
+## Args:
+##  dt(data.table): a data table
+##  noise_l(numeric): lower noise
+##  noise_u(numeric): upper noise
+##  col_excl(a vector of characters): names of columns not included which do not apply the noise
+## Return(data.table): output of a data table after adding noise
+Noise <- function(dt, noise_l = -.00001, noise_u = .00001, col_excl){
+    dim(dt)
+    dt_noise <- apply(dt[, !col_excl, with = F], 2, function(x){
+        runif(n = length(x), min = noise_l * diff(range(x)), max = noise_u * diff(range(x)))
+    })
+    dt_noise <- dt[, !col_excl, with = F] + dt_noise
+    
+    dt_noise <- data.table(dt_noise, dt[, col_excl, with = F])
+    dt <- rbind(dt, dt_noise)
+    dim(dt)
+    dt <- dt[sample(1:nrow(dt), size = nrow(dt))]
+    return(dt)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
